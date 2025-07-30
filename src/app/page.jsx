28c5365f16a0ff5a976c75f9dcafa1f98f9a9b14
MxSGameJPS <mxsgamejps@gmail.com>
2025-07-30 18:07:28 -0300

    feat: add PlanoAssinatura component with styling
    - Created a new PlanoAssinatura component to showcase subscription plans for pets.
    - Added corresponding CSS module for styling the component, including layout, typography, and button interactions.
    - Implemented a responsive design with a gradient background and shadow effects for visual appeal.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/header";
 import Hero from "../components/Hero/hero";
 import FeaturedProducts from "../components/FeaturedProducts/featuredProducts";
+import PlanoAssinatura from "../components/PlanoAssinatura/planoAssinatura";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <FeaturedProducts />
+      <PlanoAssinatura />
     </div>
   );
 }
---
+++ src/components/PlanoAssinatura/planoAssinatura.jsx
@@ -0,0 +1,27 @@
+import styles from "./planoAssinatura.module.css";
+import Image from "next/image";
+
+export default function PlanoAssinatura() {
+  return (
+    <section className={styles.planoContainer}>
+      <Image
+        src="/image/planoDeAssinatura.png"
+        alt="Plano de Assinatura"
+        width={220}
+        height={220}
+        className={styles.planoImg}
+      />
+      <div className={styles.planoInfo}>
+        <div className={styles.planoTitle}>
+          Mais diversão, menos tédio! Transforme a rotina do seu pet!
+        </div>
+        <div className={styles.planoText}>
+          Brinquedos, petiscos e novidades desenvolvidos por especialistas para
+          o perfil do seu peludo. Aproveite a edição especial com brinquedos
+          oficiais Superman.
+        </div>
+        <button className={styles.planoBtn}>Eu quero</button>
+      </div>
+    </section>
+  );
+}
